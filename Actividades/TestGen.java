fa9cabaecb081f702e644fc9101c301cfd5fb7a7
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 06:29:29 -0500

    v28

--- Actividades/TestGen.java
+++ Actividades/TestGen.java
@@ -1,3 +1,6 @@
+
+import jdk.jshell.execution.Util;
+
 public class TestGen {
     public static <T> boolean exist(T[] array, T elemnto){
         for (T item : array){
@@ -13,7 +16,10 @@ public class TestGen {
         Golosina[] golosinas={new Golosina("Caramelo", 10.5), new Golosina("Gomita", 5.2)};
         Chocolatina[] chocolatinas={new Chocolatina("Milka"), new Chocolatina("Ferrero")};
 
-        
+        System.out.println(Util.exist(golosinas, new Golosina("Caramelo", 10.5)));//true
+        System.out.println(Util.exist(chocolatinas, new Chocolatina("Oreo")));
+
+
     }
 }
 
