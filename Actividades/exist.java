d6910331e2f8eb3051c2be662c0bc7f0c31a4d8b
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 06:12:40 -0500

    v24

--- Actividades/exist.java
+++ Actividades/exist.java
@@ -1,7 +1,13 @@
 
 public class exist {
     public static <T> boolean existe(T[] array, T elemnto){
+        for (T item : array){
+            if(item.equals(elemnto)){
+                return true;
+            }
         
+        }
+        return false;
     }
 
 }
