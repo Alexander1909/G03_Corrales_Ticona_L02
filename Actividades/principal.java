f9ad7633c36b26a24bcc6b1c9f080d22aa9b8092
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-27 20:35:02 -0500

    v15

--- Actividades/principal.java
+++ Actividades/principal.java
@@ -1,7 +1,11 @@
 
 public class principal {
     public static void main(String[] args) {
-        bolsa <Chocolatina> bolsaCho = new bolsa <Chocolatina> ();
+        bolsa <Chocolatina> bolsaCho = new bolsa <Chocolatina>();
+        Chocolatina c = new Chocolatina("milka");
+        Chocolatina c1 = new Chocolatina("milka");
+        Chocolatina c2 = new Chocolatina("ferrero");
+        
 
     }
 }
