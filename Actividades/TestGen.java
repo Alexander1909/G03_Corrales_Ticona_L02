240aab8f2ea25afad6ac73a2d062d14710cc7834
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 06:21:38 -0500

    v27

--- Actividades/TestGen.java
+++ Actividades/TestGen.java
@@ -10,8 +10,10 @@ public class TestGen {
     }
 
     public static void main(String[] args) {
-        Golosina[] golosinas={new Golosina("Caramelo", 10.5)};
-        Chocolatina[] 
+        Golosina[] golosinas={new Golosina("Caramelo", 10.5), new Golosina("Gomita", 5.2)};
+        Chocolatina[] chocolatinas={new Chocolatina("Milka"), new Chocolatina("Ferrero")};
+
+        
     }
 }
 
