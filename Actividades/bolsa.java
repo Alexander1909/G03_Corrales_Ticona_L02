b5bc8f7bf59b8c1ab8971b01581114685c2adb8c
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-27 20:10:33 -0500

    v8

---
+++ Actividades/bolsa.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arraylist;
+import java.util.Iterator;
+public class bolsa< T > implements Iterable<T> {
+    private ArrayList < T > lista = new ArrayList<T>();
+    private int tope;
+
+    publi
+
+
+}
