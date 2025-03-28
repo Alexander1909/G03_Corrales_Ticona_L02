cbab4438c6afb82ad4dd890aba8617ca945753c2
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 07:14:29 -0500

    v40

--- Actividades/Cajoneria.java
+++ Actividades/Cajoneria.java
@@ -3,6 +3,15 @@ import java.util.Iterator;
 public class Cajoneria <T> implements Iterable<T> {
     private Arraylist <T> lista = new ArrayList<T>();
     private int tope;
-    
+
+    public Cajoneria(int tope){
+        super();
+        this.tope=tope;
+    }
+    public void add(T objeto){
+        if (lista.size() >= tope);
+    }else {
+        throw ner RuntimeException("no cabe mas");
+    }
 
 }
