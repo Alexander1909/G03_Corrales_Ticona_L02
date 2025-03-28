fdff7c22bf79b2dc0bd6684989f989e7c4c99755
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-27 20:43:00 -0500

    v18

--- Actividades/DemoMetodoGenerico.java
+++ Actividades/DemoMetodoGenerico.java
@@ -1,7 +1,14 @@
+
+import javax.xml.XMLConstants;
+
 public class DemoMetodoGenerico {
 
     static <T extends Comparable <T>> boolean igualArrays (T[] x, T[] y){
 
-        if 
-    }
+        if ( x.length  != y.length)
+            return false;
+        for(int i=0 ; i< x.length ; i++)
+            if()
+
+        
 }
