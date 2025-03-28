4c455093bf54b4a6393c715c687b7fb37f9526db
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 08:22:52 -0500

    v53

--- Actividades/cajoneria/Cajoneria.java
+++ Actividades/cajoneria/Cajoneria.java
@@ -1,44 +1,8 @@
 package cajoneria;
-import java.util.ArrayList;
-import java.util.Iterator;
-public class Cajoneria <T> implements Iterable<T> {
-    private Arraylist <T> lista = new ArrayList<T>();
-    private int tope;
+class Cajoneria<T> {
+    private ArrayList<Caja<T>> cajas = new ArrayList<>();
 
-    public Cajoneria(int tope){
-        super();
-        this.tope=tope;
-    }
-    public void add(T objeto){
-        if (lista.size() >= tope);
-    }else {
-        throw ner RuntimeException("no cabe mas");
+    public void addCaja(Caja<T> caja) {
+        cajas.add(caja);
     }
 
-    public Iterator < T > iterator(){
-        return lista.iterator();
-    }
-    // Metodo Buscar
-    public String search(T objeto){
-        int index= lista.indexOF(objeto);
-        if (index != -1) {
-            return "Objeto encontrado en posición " + (index + 1);
-        }
-        return "Objeto no encontrado";
-    
-    }
-    // Metodo Borrar
-    public T delete(T objeto){
-        return lista.remove(objeto) ? objeto : null;
-    }
-    // Metodo toString
-    @Override
-    public String toString(){
-        StringBuilder resultado = new StringBuilder("Contenido de la cajonería:\n");
-        for (int i = 0; i < lista.size(); i++) {
-            resultado.append((i + 1)).append(". ").append(lista.get(i).toString()).append("\n");
-        }
-        return resultado.toString(); 
-
-    }
-}
