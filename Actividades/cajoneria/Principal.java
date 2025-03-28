816335db7edfdfe736262c26e88e365b2a3eed6d
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-28 07:20:28 -0500

    v43

---
+++ Actividades/cajoneria/Principal.java
@@ -0,0 +1,10 @@
+package cajoneria;
+
+public class Principal {
+    public static void main(String[] args) {
+        Cajoneria <Caja> cajon = new Cajoneria<>();
+
+        Caja c1= new Caja(color, cajon)
+    }
+
+}
