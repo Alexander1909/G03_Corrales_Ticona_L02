92726fce05252bbf3c2bc5641fe1587efa9ec10e
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-27 20:47:27 -0500

    v19

--- Actividades/DemoMetodoGenerico.java
+++ Actividades/DemoMetodoGenerico.java
@@ -8,7 +8,17 @@ public class DemoMetodoGenerico {
         if ( x.length  != y.length)
             return false;
         for(int i=0 ; i< x.length ; i++)
-            if()
+            if(!x[i].equals(y[i]))
+            return false;
+        return true;        
+}
 
-        
+public static void main(String[] args) {
+    Integer nums[]={1,2,3,4,5};
+    Integer nums2[]={1,2,3,4,5};
+    Integer nums3[]={1,2,7,4,5};
+    Integer nums4[]={1,2,7,4,5,6};
+    if(igualArrays(nums, nums)){
+        System.err.println("nums es igual a nums");
+    }
 }
