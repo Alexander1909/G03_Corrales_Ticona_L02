c1b5acbfeae94b9de8688df27e1b937c5975c33d
Alexander1909 <131818021+Alexander1909@users.noreply.github.com>
2025-03-27 20:15:35 -0500

    v10

--- Actividades/bolsa.java
+++ Actividades/bolsa.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arraylist;
 import java.util.Iterator;
 public class bolsa< T > implements Iterable<T> {
     private ArrayList < T > lista = new ArrayList<T>();
@@ -12,7 +11,11 @@ public class bolsa< T > implements Iterable<T> {
 
     public void add(T objeto){
         if (lista.size() >= tope);
+    }else {
+        throw ner RuntimeException("no cabe mas");
     }
-
+    public Iterator < T > iterator(){
+        return list.iterator();
+    }
 
 }
